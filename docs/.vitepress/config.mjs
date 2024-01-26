0cbd93ba6b7c357fe08c0e41d566621c89c0bf9f
HuoTianCi <2737684365@qq.com>
2024-01-26 18:11:29 +0800

    更新

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,18 +2,30 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "My Awesome Project",
+  title: "HuoTianCi",
+  titleTemplate: "记录",
   description: "A VitePress Site",
+  head: [["link", { rel: "icon", href: "/title.svg" }]],
+  base: "/",
+  lastUpdated: true, // 最后更新时间
+  scrollOffset: "header", // 滚动偏移
+  cleanUrls: true, // 干净路由
   themeConfig: {
+    // 配置头像
+    logo: "./logo.jpg",
+    search: {
+      provider: "local",
+    },
+    i18nRouting: true,
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
+      { text: '首页', link: '/' },
       { text: 'Examples', link: '/markdown-examples' }
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: '案列',
         items: [
           { text: 'Markdown Examples', link: '/markdown-examples' },
           { text: 'Runtime API Examples', link: '/api-examples' }
@@ -21,8 +33,15 @@ export default defineConfig({
       }
     ],
 
+
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+      { icon: 'https://e-assets.gitee.com/gitee-community-web/_next/static/media/logo-white.a5b0e29c.svg', link: 'https://gitee.com/huotianci' }
+    ],
+    //配置页脚
+    footer: {
+      message: "WeChat：Huo273768365",
+      copyright: "Huo 天赐",
+    },
   }
 })
